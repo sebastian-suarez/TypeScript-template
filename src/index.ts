dc7eb089f5b6425e6971d14d752b21f5109f91cd
Sebastian Suarez <sebastian.suarez@Sebastians-MacBook-Pro.local>
2025-10-24 12:04:17 -0500

    Add env variables

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import process from "node:process";
 import greet from "#src/modules/greet.js";
 
+console.debug(`KEY=${process.env.KEY}`);
 console.log(greet("World"));
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,8 @@
+import process from "node:process";
 import test from "ava";
 import greet from "#src/modules/greet.ts";
 
 test("Greet", (t) => {
+	console.debug(`KEY=${process.env.KEY}`);
 	t.is(greet("World"), "Hello, World!");
 });
